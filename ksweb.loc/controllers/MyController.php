b904cbef2982b2e424f9eafe4b0db75f62e918bc
igor-makariew <34421941+igor-makariew@users.noreply.github.com>
2018-03-24 14:19:03 +0300

    24.03.2018
    MyController

--- ksweb.loc/controllers/MyController.php
+++ ksweb.loc/controllers/MyController.php
@@ -26,18 +26,21 @@ class MyController extends Controller {
                 ]);
     }
     
-    public function actionDownload($filename) {
+    public function actionDownload() {
         
         $this->view->title = 'Загрузка';
         
-        $storagePath = Yii::getAlias('@app/files');
-
+        $storagePath = Yii::getAlias('@app/files/');
+       
         // check filename for allowed chars (do not allow ../ to avoid security issue: downloading arbitrary files)
-        if (!preg_match('/^[a-z0-9]+\.[a-z0-9]+$/i', $filename) || !is_file("$storagePath/$filename")) {
+        if (!preg_match('/^[a-z0-9]+\.[a-z0-9]+$/i', '7.jpg') || !is_file($storagePath.'7.jpg')) {
             throw new \yii\web\NotFoundHttpException('The file does not exists.');
         }
-        $files = Yii::$app->response->sendFile("$storagePath/$filename", $filename)->send();
-        return $this->render('download', compact('files'));
+        $files = Yii::$app->response->sendFile($storagePath.'7.jpg', '7.jpg')->send();
+        return $this->render('download');
+        //return Yii::$app->response->sendFile('C:\OSPanel\domains\ksweb.loc\files\88.jpg'); 
+//        return Yii::$app->response->sendFile($storagePath.'7.jpg', '7.jpg'); 
+//        return $this->render('download');
     }
 
 }    
--- ksweb.loc/views/my/download.php
+++ ksweb.loc/views/my/download.php
@@ -4,5 +4,50 @@ use yii\helpers\Url;
 
 ?>
 
-<h1>Hello world and girl</h1>
-<?= Html::a('BUSINESS OFFERS', Url::to(['/bussines-offers']))?>
+<h1 align="center">Hello world and girl</h1>
+<span class="url"><?= Html::a('BUSINESS OFFERS', Url::to(['/bussines-offers']))?></span>
+<br>
+<?= Html::img('/files/7.jpg', ['alt' => 'GIRLS']);?>
+
+<br>
+<?= Html::img('/web/img/9.jpg ', ['alt' => 'KSWeb'])?>
+
+
+<!--<div class="slider">
+		<div class="">
+			<div class="">
+				<div class="responsive-slider" data-spy="responsive-slider" data-autoplay="true">
+					<div class="slides" data-group="slides">						
+						<ul>
+							<li>
+								<div class="slide-body" data-group="slide">
+                                                                        <?= Html::img('/web/img/3.jpg', ['alt' => 'KSWeb'])?>
+                                                                    <img src="/web/img/3.jpg" alt="" class="img-responsive" > 
+								</div>					
+							</li>
+							<li>
+								<div class="slide-body" data-group="slide">
+                                                                        <?= Html::img('/files/4c.jpg', ['alt' => 'GIRLS'])?>
+									<img src="img/6.jpg" alt="" class="img-responsive" >  
+								</div>
+							</li>
+							<li>
+								<div class="slide-body" data-group="slide">
+                                                                        <?= Html::img('/web/img/5.jpg', ['alt' => 'KSWeb'])?>
+									<img src="img/5.jpg" alt="" class="img-responsive" >  									
+								</div>
+							</li>
+                                                        <li>
+								<div class="slide-body" data-group="slide">
+                                                                        <?= Html::img('/files/8e8.jpg', ['alt' => 'GIRLS'])?>
+									<img src="img/5.jpg" alt="" class="img-responsive" >  									
+								</div>
+							</li>		
+						</ul>
+					</div>			   
+					<a class="slider-control left" href="#" data-jump="prev"><i class="fa fa-angle-left fa-3x"></i></a>
+					<a class="slider-control right" href="#" data-jump="next"><i class="fa fa-angle-right fa-3x"></i></a>		
+				</div>
+			</div>
+		</div>
+	</div>-->
