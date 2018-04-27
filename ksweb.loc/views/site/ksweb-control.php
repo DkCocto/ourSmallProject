db83b939689ca5e56b78c27a40fa066b94e9acf7
igor-makariew <34421941+igor-makariew@users.noreply.github.com>
2018-04-27 12:54:54 +0300

    new site 27.04.2018
    добален файл ksweb-control.php

--- ksweb.loc/views/site/ksweb-control.php
+++ ksweb.loc/views/site/ksweb-control.php
@@ -20,6 +20,32 @@ $this->params['breadcrumbs'][] = $this->title;
             The typical usage is to extend this class by your main activity. Each command requires a tag for tracking of the result of its execution. KSWEB sends an answer with a tag via 
             Broadcast after executing any command. It can be KSWEBControl.RESPOND_OK or KSWEBControl.RESPOND_ERROR.</p>
                 
+        
+            <p>Here you can see a list of available commands to control KSWEB.</p>
+            <p><strong>This group of static commands allows you to start/stop components (servers) of KSWEB</strong></p>
+            <p><code>public static void ftpStart(Activity activity, String tag);<br />
+            public static void ftpStop(Activity activity, String tag);</p>
+            <p>public static void lighttpdStart(Activity activity, String tag);<br />
+            public static void lighttpdStop(Activity activity, String tag);</p>
+            <p>public static void nginxStart(Activity activity, String tag);<br />
+            public static void nginxStop(Activity activity, String tag);</p>
+            <p>public static void mysqlStart(Activity activity, String tag);<br />
+            public static void mysqlStop(Activity activity, String tag);</code></p>
+            <p><strong>These commands allow to start/close KSWEB. &#8220;Finish activity&#8221; allows to unload KSWEB from memory without shutting down servers</strong></p>
+            <p><code>public static void kswebStart(Activity activity, String tag);<br />
+            public static void kswebClose(Activity activity, String tag);<br />
+            public static void kswebFinishActivity(Activity activity, String tag);</code></p>
+            <p><strong>To configure servers use these commands</strong></p>
+            <p>Content of configuration file will be replaced by data from configTxt parameter.</p>
+            <p><code>public static void lighttpdSetConfig(Activity activity, String tag, String configTxt);<br />
+            public static void nginxSetConfig(Activity activity, String tag, String configTxt);<br />
+            public static void phpSetConfig(Activity activity, String tag, String configTxt);<br />
+            public static void mysqlSetConfig(Activity activity, String tag, String configTxt);</code></p>
+            <p><strong>To control http servers host list you can use these commands</strong></p>
+            <p><code>public static void lighttpdAddHost(Activity activity, String tag, String hostname, String port, String rootDir);<br />
+            public static void lighttpdDeleteHost(Activity activity, String tag, String hostname, String port, String rootDir);</p>
+            <p>public static void nginxAddHost(Activity activity, String tag, String hostname, String port, String rootDir);<br />
+            public static void nginxDeleteHost(Activity activity, String tag, String hostname, String port, String rootDir);</code></p>
         </div>
     </div>                
 
